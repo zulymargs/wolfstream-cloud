d4b42d13292535c58faee18f56ff99d0fcfaba16
zulymargs <zulymar.garcia@upr.edu>
2024-02-20 19:10:12 -0400

    Fixed homeviewer

---
+++ wolf-stream/client/src/components/headerViewer.jsx
@@ -0,0 +1,34 @@
+// Header.js
+
+import React from 'react';
+
+const HeaderViewer = ({searchVideo }) => {
+  return (
+    <header className="header">
+      <div className="botones-header">
+        <a href="#" className="avatar">
+          <img src="Fotos/guest-user.jpg" alt="perfil" />
+        </a>
+        <a href="">
+          <button id="home">
+            <span className="material-symbols-outlined">home</span>
+          </button>
+        </a>
+      </div>
+
+      <div className="barra-busqueda">
+        <input
+          id="searchbar"
+          onChange={(e) => searchVideo(e.target.value)}
+          type="text"
+          name="search"
+          placeholder="Search"
+        />
+      </div>
+
+      
+    </header>
+  );
+};
+
+export default HeaderViewer;
--- wolf-stream/client/src/components/homeViewer.jsx
+++ wolf-stream/client/src/components/homeViewer.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Header from './headerViewer'; // Import the Header component
 
 const HomeViewer = () => {
   const contenedorRef = useRef(null);
   const [anchoVentana, setAnchoVentana] = useState(window.innerWidth);
   const [videos, setVideos] = useState([]);
   const navigate = useNavigate();
+
+  const [filteredVideos, setFilteredVideos] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('All');
 
   useEffect(() => {
     const handleResize = () => {
@@ -20,12 +24,12 @@ const HomeViewer = () => {
   }, []);
 
   useEffect(() => {
-    // Simulando la obtención de datos desde una base de datos
     const fetchDataFromDatabase = async () => {
       try {
-        const response = await fetch("http://localhost8000/home"); // Reemplaza 'URL_DEL_BACKEND' con la URL real de tu endpoint en el backend
+        const response = await fetch('http://localhost:8000/home/videos');
         const data = await response.json();
         setVideos(data);
+        setFilteredVideos(data); // Initialize filteredVideos with all videos
       } catch (error) {
         console.error("Error fetching data from the database:", error);
       }
@@ -44,45 +48,32 @@ const HomeViewer = () => {
     }
   }, [anchoVentana]);
 
-  // search bar & category filter code
-
   const searchVideo = (val) => {
-    const input = val || document.getElementById("searchbar").value;
-    const x = document.getElementsByClassName("video");
+    const input = val ? val.toLowerCase() : '';
+    let filtered;
 
-    for (let i = 0; i < x.length; i++) {
-      // Obtén el texto del elemento excluyendo los elementos h4 y p
-      const videoText = Array.from(x[i].childNodes)
-        .filter((node) => node.nodeName !== "H4" && node.nodeName !== "P")
-        .map((node) => node.textContent.toLowerCase())
-        .join(" ");
-
-      // Compara el texto con la entrada del usuario
-      if (!videoText.includes(input.toLowerCase())) {
-        x[i].style.display = "none";
-      } else {
-        x[i].style.display = "list-item";
-      }
-    }
-  };
-
-  const category = (par) => {
-    if (par === "category") {
-      searchVideo();
-    } else if (
-      par === "kids" ||
-      par === "vehicles" ||
-      par === "sport" ||
-      par === "music"
-    ) {
-      searchVideo(par);
+    if (input.trim() === 'category' && selectedCategory === 'All') {
+      filtered = videos;
     } else {
-      searchVideo();
+      filtered = videos.filter((video) => {
+        const videoText = `${video.title} ${video.description} ${video.category.join(' ')}`;
+        return (
+          videoText.toLowerCase().includes(input) &&
+          (selectedCategory === 'All' || video.category.includes(selectedCategory))
+        );
+      });
     }
+
+    setFilteredVideos(filtered);
   };
 
   const handlePlayback = (videoId) => {
-    navigate(`/playback/${videoId}`);
+    console.log('Video ID:', videoId._id);
+    navigate(`/playback/${videoId._id}`);
+  };
+
+  const handleUpload = () => {
+    navigate("/upload");
   };
 
   return (
@@ -96,44 +87,14 @@ const HomeViewer = () => {
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
       />
 
-      <div
-        className="contenedor"
-        ref={contenedorRef}
-        style={{ height: "600px" }}
-      >
-        <header className="header">
-          <div className="botones-header">
-            <a href="#" className="avatar">
-              <img src="Fotos/guest-user.jpg" alt="perfil" />
-            </a>
-            <a href="">
-              <button id="home">
-                <span className="material-symbols-outlined">home</span>
-              </button>
-            </a>
-            <a href="#">
-              <button id="upload">
-                <span className="material-symbols-outlined">upload</span>
-              </button>
-            </a>
-          </div>
+      <div className="contenedor" ref={contenedorRef} style={{ height: '600px' }}>
+        {/* Use the Header component */}
+        <Header handleUpload={handleUpload} searchVideo={searchVideo} />
 
-          <div className="barra-busqueda">
-            <input
-              id="searchbar"
-              onChange={() => searchVideo()}
-              type="text"
-              name="search"
-              placeholder="Search"
-            />
-          </div>
-
+        <main className="main">
+          <h3 className="titulo">Wolf Stream</h3>
           <div className="contenedor-logo">
-            <select
-              name="categorias"
-              className="categorias"
-              onChange={(e) => category(e.target.value)}
-            >
+            <select name="categorias" className="categorias" onChange={(e) => searchVideo(e.target.value)}>
               <option value="category">Categories</option>
               <option value="kids">Kids</option>
               <option value="sport">Sport</option>
@@ -141,31 +102,21 @@ const HomeViewer = () => {
               <option value="music">Music</option>
             </select>
           </div>
-        </header>
-
-        <main className="main">
-          <h3 className="titulo">Main Page</h3>
           <div className="grid-videos">
-            {/* Dynamic rendering of videos */}
-            {videos.map((video) => (
-              <a
-                key={video._id}
-                onClick={handlePlayback(video._id)}
-                className="video"
-              >
+            {filteredVideos.map((video) => (
+              <div key={video._id} className="video" onClick={() => handlePlayback(video)}>
                 <video poster={video.poster} className="video">
                   <source src={video.src} type="video/mp4" />
-                  Tu navegador no admite el elemento de video.
+                  Your browser does not support the video tag.
                 </video>
                 <h4 className="title">{video.title}</h4>
-              </a>
+              </div>
             ))}
           </div>
         </main>
 
         <footer>
-          &#169; Group 3 and 4 <br /> Zulymar García, Eddy Figueroa, Ismael
-          López and Dereck Declet
+          &#169; Group 3 and 4 <br /> Zulymar García, Eddy Figueroa, Ismael López, and Dereck Declet
         </footer>
       </div>
     </>
@@ -173,24 +124,3 @@ const HomeViewer = () => {
 };
 
 export default HomeViewer;
-
-//  <a href="playback.html?video_id=1" className="video"><video poster="Fotos/manSurfing.PNG"  className="video" >sport surf</video><h4 className="title">Man Surfing</h4></a>
-//             <a href="playback.html?video_id=2" className="video"><video poster="Fotos/motocross.PNG"  className="video">sports vehicles motorcycle bikes</video><h4 className="title">Mountain Motocross</h4></a>
-//             <a href="playback.html?video_id=3" className="video"><video poster="Fotos/bicicleta.PNG"  className="video">sports vehicles bicycle bikes</video><h4 className="title">Bicycle street run </h4></a>
-//             <a href="playback.html?video_id=4" className="video"><video poster="Fotos/futbol1.PNG"  className="video">sports futbol</video><h4 className="title">Free kick</h4></a>
-//             <a href="playback.html?video_id=5" className="video"><video poster="Fotos/cars1.PNG"  className="video">vehicles cars</video><h4 className="title">Luxury Cars</h4></a>
-//             <a href="playback.html?video_id=6" className="video"><video poster="Fotos/cars2.PNG"  className="video">vehicles cars</video><h4 className="title">McLaren MP4</h4></a>
-//             <a href="playback.html?video_id=7" className="video"><video poster="Fotos/kids1.PNG"  className="video">kids </video><h4 className="title">Kids study</h4></a>
-//             <a href="playback.html?video_id=8" className="video"><video poster="Fotos/basket.PNG"  className="video">sports basketball</video><h4 className="title">Basketball practice</h4></a>
-//             <a href="playback.html?video_id=9" className="video"><video poster="Fotos/kids2.PNG"  className="video">kids music</video><h4 className="title">Kid playing drums</h4></a>
-//             <a href="playback.html?video_id=10" className="video"><video poster="Fotos/kids3.PNG" className="video">kids</video><h4 className="title">Birthday Party</h4></a>
-//             <a href="playback.html?video_id=11" className="video"><video poster="Fotos/cars3.PNG" className="video">vehicles cars</video><h4 className="title">Run in dunes</h4></a>
-//             <a href="playback.html?video_id=12" className="video"><video poster="Fotos/skyDiving.PNG" className="video">sports skydiving</video><h4 className="title">Sky Diving</h4></a>
-//             <a href="playback.html?video_id=13" className="video"><video poster="Fotos/baseball.PNG" className="video">sports baseball </video><h4 className="title">Baseball Park</h4></a>
-//             <a href="playback.html?video_id=14" className="video"><video poster="Fotos/futbol2.PNG" className="video">sports futbol </video><h4 className="title">Futbol Stadium</h4></a>
-//             <a href="playback.html?video_id=15" className="video"><video poster="Fotos/motocross2.PNG" className="video">sports vehicles motorcycle bikes</video><h4 className="title">Circuit Motocross</h4></a>
-//             <a href="playback.html?video_id=16" className="video"><video poster="Fotos/tenis.PNG" className="video">sports tennis</video><h4 className="title">Tennis game</h4></a>
-//             <a href="playback.html?video_id=17" className="video"><video poster="Fotos/futbol3.PNG" className="video">sports futbol</video><h4 className="title">Futbol sky shot</h4></a>
-//             <a href="playback.html?video_id=18" className="video"><video poster="Fotos/bike.PNG" className="video">vehicles motorcycle bikes</video><h4 className="title">Motorcycle run</h4></a>
-//             <a href="playback.html?video_id=19" className="video"><video poster="Fotos/kids4.PNG" className="video">kids</video><h4 className="title">Raining Day</h4></a>
-//             <a href="playback.html?video_id=20" className="video"><video poster="Fotos/arrow.PNG" className="video">sports bow and arrow</video><h4 className="title">Bow & Arrow shot</h4></a>
